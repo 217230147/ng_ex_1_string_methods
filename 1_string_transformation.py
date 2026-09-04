d1dadacafda53b08a22c299fcdf11ba36d6ff826
prabhatram <prabhatdreamz@gmail.com>
2026-09-04 08:53:42 +0800

    non-graded exercise 1

---
+++ 1_string_transformation.py
@@ -0,0 +1,15 @@
+booking = "   EVT-2026 | alice_wong | Room-305 | 14:30 | alice.wong@UniMail.edu | VIP-VIP   "
+
+######### EXPECTED OUTPUT #########
+""" Event code: EVT-2026
+Name: Alice_Wong
+Room: ROOM-305
+Time: 14:30
+Email domain: unimail.edu
+VIP tag count: 2
+Valid event code: True
+Valid username: True
+Valid room: True
+Valid time: True
+Valid email: True """
+
---
+++ 2_decipher.py
@@ -0,0 +1,21 @@
+encoded = """
+   !!junk-77!! | [3::DW::ok] | [xx::DRSC::bad] |
+   [1::NFFU::ok] | ##nothing## | [5::TQI_QNGWFWD::ok] |
+   [2::OG::ok] | [4::XLI::ok] | [7::WT7::bad] |
+   [6::GZ_7_VS::ok] | [99::IGNORE_ME::bad] | %%noise%%
+"""
+
+###############################################################
+"""
+1. Part of the real message is inside the the '[' and ']' brackets.
+2. Each fragment inside the brackets has a number, jumbled text of the message, and 'ok'. Focus on only those fragments. The '::' are just separating these parts in the fragment 
+3. To find the actual message in every fragment,take every letter in the jumbled message, and shift it backward by the number part in that fragment
+For example, if the number is 3 and the jumbled message is ABC, then the actual message is XYZ.
+Similarly, if the number is 5 and the jumbled message is ABC, then the actual message is VWX.
+4. Ignore any fragment that has 'bad' instead of 'ok'.
+5. Once you have decoded all the fragments, combine them in the order of their numbers to get the final message. First comes the fragment with number 1, then 2, and so on.
+"""
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
